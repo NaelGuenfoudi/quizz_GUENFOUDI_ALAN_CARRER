3e3c3d323213ae70018fa85c8e09b38f23ab2782
NaelGuenfoudi <guenfmen@gmail.com>
2023-09-07 14:43:50 +0200

    Ajout de deux fichiers

---
+++ src/dal.py
@@ -0,0 +1,136 @@
+import mysql.connector
+from mysql.connector import Error
+from configparser import ConfigParser
+
+def read_db_config(filename='src/db/conf.ini', section='MySQL'):
+        parser = ConfigParser()
+        parser.read(filename)
+        
+        db_config = {}
+        if parser.has_section(section):
+            params = parser.items(section)
+            for param in params:
+                db_config[param[0]] = param[1]
+        else:
+            raise Exception(f'Section {section} not found in the {filename} file')
+        
+        return db_config
+
+  
+def create_connection():
+        db_config = read_db_config()
+        try:
+            connection = mysql.connector.connect(**db_config) # Utilisation de ** pour envoyer les paramètres de connexion contenus dans db_confi
+            
+            return connection
+        except Error as e:
+            print("Error connecting to MySQL:", e)
+            return None
+        
+def select_query(query, params=None):
+    # Assuming DataBas_connection() is a function that gives a database connection.
+    conn = create_connection()
+    cursor = conn.cursor()
+    try:
+        # If no params are passed, we'll default to an empty tuple
+        params = params or ()
+        cursor.execute(query)
+
+        # We might want to decide if we need to fetch data based on query type
+        results = cursor.fetchall()
+        return results
+       
+
+    except Exception as e:
+        print(f"Error executing query: {e}")
+        # In the case of an error, you might want to return None or raise the error, depending on your requirements.
+        return None
+
+    finally:
+        if conn.is_connected():
+            conn.close()
+            print('close')
+
+
+def getAllPlayersData():
+    """
+    Récupère les données de tous les joueurs.
+
+    Retour:
+        list: Liste de tuples contenant les données des joueurs.
+    """
+    players_data = select_query('select * from joueur')
+    return players_data
+
+def getAllQuestionData():
+    """
+    Récupère toutes les données des questions.
+
+    Retour:
+        list: Liste de tuples contenant les données des questions.
+    """
+    questions_data= select_query('select * from Question ')
+    return questions_data
+
+def getGoodResponseForQuestion(idQuestion):
+    """
+    Récupère la bonne réponse pour une question donnée.
+
+    Args:
+        idQuestion (int): L'identifiant de la question.
+
+    Retour:
+        list: Liste contenant le tuple de la bonne réponse.
+    """
+    return select_query(f"select ID_Reponse from Reponse where Est_Correcte=true and ID_Question={idQuestion}")
+
+def getAllTheme():
+    """
+    Récupère tous les thèmes.
+
+    Retour:
+        list: Liste de tuples contenant l'ID et le nom des thèmes.
+    """
+    return select_query('select ID_THEME,Nom from Theme')
+
+def getAllLevel():
+    """
+    Récupère tous les niveaux disponibles.
+
+    Retour:
+        list: Liste de tuples contenant l'ID et le nom des niveaux.
+    """
+    return [(1, "Niveau facile"), (2, "Niveau dur"), (3, "Niveau expert")]
+
+def getQuestionsOfThemeAndLevel(idTheme, idLevel):
+    """
+    Récupère les questions selon un thème et un niveau donnés.
+
+    Args:
+        idTheme (int): L'identifiant du thème.
+        idLevel (int): L'identifiant du niveau.
+
+    Retour:
+        list: Liste de tuples contenant l'ID et le texte des questions.
+    """
+    query = f'select ID_Question, Texte from Question where ID_Theme={idTheme} and ID_Level={idLevel}'    
+    return select_query(query)
+
+
+def main():
+    print(select_query('select * from Question where ID_Question=3'))
+
+main()
+
+
+
+
+    
+
+
+
+
+
+
+
+
--- src/main.py
+++ src/main.py
@@ -1,6 +1,29 @@
-from model.Joueur import *
+from dal import *
+
+def choisirNiveau():
+    levels=getAllLevel()
+    print (levels)
+    print('voici tous les niveau:')
+    for id, (num, nom) in enumerate(levels):
+        print(str(id) + ': ' + nom)
+
+    niveauChoisi=None
+    goodNumero=False
+
+    while not goodNumero:
+        niveauChoisi=input('Veuillez choisir le niveau voulu en y donnant son numero')
+        if niveauChoisi.isdigit() and 0 <= int(niveauChoisi) < len(levels):
+            goodNumero = True
+        else:
+            print("Numéro invalide.")
+    
+    return levels[int(niveauChoisi)-1]
 
 def main():
-    abdel=Joueur(Nom='Abdel',Prenom='Abdelou')
-    abdel.save()
-    Joueur.getAllPlayers()
+   niveauChoisi=choisirNiveau()
+   idThemeChoisi=1
+   questions=getQuestionsOfThemeAndLevel(idThemeChoisi,niveauChoisi)
+   
+
+main()
+
