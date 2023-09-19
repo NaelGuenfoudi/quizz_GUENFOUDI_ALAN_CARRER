c1d1ebf3b12cb59a4c351bfab89821da5bc0d266
NaelGuenfoudi <guenfmen@gmail.com>
2023-09-19 13:44:31 +0200

    Implementation du code de richard pour le timer dans les questions

--- src/Game.py
+++ src/Game.py
@@ -1,49 +1,38 @@
-from timer import *
+
 import sys
-import threading
+
 from dal import *
+import time
+import os
+import sys
+from multiprocessing import Event, Manager, Process
+from ctypes import c_char_p
 
 
 class Game:
-    def __init__(self, total_questions=10, time_per_question=15, total_time=60):
-        self.total_questions = total_questions
-        self.time_per_question = time_per_question
-        self.total_time = total_time
+
+    TOTAL_TIME=60
+    QUESTION_TIME=10
+    TOTAL_QUESTION=60
+    def __init__(self, ):
+       
+        
         self.current_reponse = 0
         self.current_question_number = 0
         self.game_over = False
         self.question_over = False
         self.current_timer_question = None
         self.current_timer_global = None
+        isFinish=None
 
-    def start_timer_question(self, duration, callback):
-        """Lance un timer qui s'exécute en arrière-plan."""
-        self.current_timer_question = None
-        self.current_timer_question = threading.Timer(duration, callback)
-        self.current_timer_question.start()
-
-    def start_timer_global(self, duration, callback):
-        """Lance un timer qui s'exécute en arrière-plan."""
-        self.current_timer_question = None
-        self.current_timer_global = threading.Timer(duration, callback)
-        self.current_timer_global.start()
-
-    def close_timer_question(self):
-        """Annule le timer actuel s'il est en cours d'exécution."""
-
-        self.current_timer_question.cancel()
-
-    def time_for_question_is_up(self):
-
-        self.question_over = True
+    
 
     def time_global_is_up(self):
         print("\nTemps global écoulé!")
         self.game_over = True
 
     def next_question(self):
-        print('next_question')
-        if not self.game_over and self.current_question_number < self.total_questions:
+        if not self.game_over and self.current_question_number < Game.TOTAL_QUESTION:
             self.current_question_number += 1
             self.ask_question()
         else:
@@ -52,53 +41,123 @@ class Game:
     def afficherReponses(self, reponses):
         for i, reponse in enumerate(reponses):
             print(f"{i + 1}. {reponse[1]}")
-
-    def ask_question(self):
-        print('ask_question')
-        questionCurrent = getTextQuestionId(self.current_question_number)
-        print(f"\nQuestion {self.current_question_number}: "+questionCurrent)
-        # le but est de recuperer toutes les reponses de la question et de les afficher, mais je ne sais pas comment on gere ,aide moi gpt
-        reponses = getReponsesForQuestion(self.current_question_number)
-
-        self.afficherReponses(reponses)
-        # Ici, nous lançons un timer pour cette question
-        self.start_timer_question(10, self.time_for_question_is_up)
-
-    def get_user_input(self):
-        answerValid=True
-        while not answerValid:
-            reponse_choisi = input("Votre réponse (1, 2 ou 3): ")
-            if self.user_input.isdigit() and 1 <= int(self.user_input) <= 3:
-                self.current_reponse=reponse_choisi
-                break
-
-    def isGoodReponse(self):
-        id_good_reponse = getGoodResponseForQuestion(
-            self.current_question_number)
-        return id_good_reponse == self.current_reponse
+    
+    def isGoodReponse(text_response,id_current_question):
+        id_current_response=get_id_reponse_for_text(text_response)
+        id_good_reponse = getGoodResponseForQuestion(id_current_question)
+        
+        return id_good_reponse==id_current_response 
 
     def getGoodIdAnswerForCurrentQuestion(self, choice):
         return getReponsesForQuestion(self.current_question_number)[choice-1][0]
 
-    def main(self):
-        # Lancer le timer global pour l'ensemble du jeu.
-        self.start_timer_global(self.total_time, self.time_global_is_up)
+    def input_question(fn, hasAnwser, res):
+        # Connexion entre la console principale et la console du process
+        sys.stdin = os.fdopen(fn)
 
-        # Boucle d'attente de réponse de l'utilisateur
-        while not self.game_over:
-            answerIsValid = False
+        # Récupération de la valeur
+        userInput = input('Donne la réponse de la question: ')
+
+
+        # On verifie que l'input n'est pas vide
+        if (userInput == ''):
+            print('Vous devez saisir une réponse correct !')
+
+            # On relance la fonction de saisie
+            Game.input_question(fn, hasAnwser, res)
+        elif userInput not in ['1', '2', '3']:
+            print('Veuillez saisir une réponse valide (1, 2 ou 3) !')
+            # On relance la fonction de saisie
+            Game.input_question(fn, hasAnwser, res)    
+        else:
+            # la valeure n'est pas vide on peut donc remplir la variable partageable
+
+            res.value = userInput
+
+
+            # Lancement de l'event pour signaler au timer qu'une valeure à été recupérée
+            hasAnwser.set()
+
+                
+
+    def ask_question(self):
+        questionCurrent = getTextQuestionId(self.current_question_number)
+        print(f"\nQuestion {self.current_question_number}: "+questionCurrent)
+        # le but est de recuperer toutes les reponses de la question et de les afficher, mais je ne sais pas comment on gere ,aide moi gpt
+        
+        reponses = getReponsesForQuestion(self.current_question_number)
+
+        self.afficherReponses(reponses)
+
+        hasAnwser = Event()
+
+        fn = sys.stdin.fileno()
+
+            # Création d'une variable partageable entre les process
+        res = Manager().Value(c_char_p, "")
+
+            # Création du process
+        processInput = Process(target=Game.input_question, args=[fn, hasAnwser, res])
+
+            # Définition du nombre de secondes à attendre
+        timeout=Game.QUESTION_TIME
+
+            # Lancement du process
+        processInput.start()
+
+            # Attente d'une réponse en fonction du temps défini sur timeout
+        hasAnwser.wait(timeout)
+
+            # Destruction du process
+        processInput.terminate()
+
+            # Si aucune valeur n'est entrée, cela signifie que le temps est écoulé
+        print(f'Value de la réponse:{res.value}')    
+        if (res.value == ''):
+            print('\nTemps écoulé !') 
+        else :
+            reponse=int(res.value)-1
+            text_reponse=reponses[reponse][1]
+            if Game.isGoodReponse(text_reponse,self.current_question_number):
+                print('ca gagne fort')
+            else:
+                print('trompé de réponse gros béta')
+        return res.value
+
+
+    
+
+    
+  
+    def global_timer(isFinish):
+        time.sleep(Game.TOTAL_TIME)  
+        if not isFinish.is_set():
+            print("\nLe timer global est écoulé !")
+            isFinish.set()
+            os._exit(0) 
+
+    
+
+    def main(self):
+        isFinish = Event()
+        self.isFinish=isFinish
+
+        # Lancement du timer global
+        processTimer = Process(target=Game.global_timer, args=[isFinish])
+        processTimer.start()
+
+        # Récupération du paramètre de connexion de la console principale
+        fn = sys.stdin.fileno()
+
+        # Tant que le timer global n'est pas terminé
+        while not isFinish.is_set():
             # Poser la première question
             self.next_question()        
-            thread_input=threading.Thread(target=self.get_user_input)
-            thread_input.start()
-            
            
-            self.close_timer_question()
-            if self.isGoodReponse():
-                print("ça gagne")
-            print(self.question_over)
-            self.question_over = False
+            
 
 
-game = Game()
-game.main()
+if __name__ == '__main__':
+    game = Game()
+    game.main()
+
