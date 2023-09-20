4ab957116783e7ff8f8f7fa483358b5c2833e183
NaelGuenfoudi <guenfmen@gmail.com>
2023-09-20 18:16:47 +0200

    ajout d un systeme de vie et d un compteur de temps par question

--- src/Game.py
+++ src/Game.py
@@ -7,6 +7,7 @@ import os
 import sys
 from multiprocessing import Event, Manager, Process
 from ctypes import c_char_p
+import math
 
 
 class Game:
@@ -23,26 +24,35 @@ class Game:
         self.current_question_number = 0
         self.game_over = False
         self.question_over = False
-        self.current_timer_question = None
-        self.current_timer_global = None
         isFinish=None
         self.score_max=0
+        self.current_time_question=0
+        self.global_time=0
+        self.nbVie=3
 
     
 
    
 
     def next_question(self):
-            self.current_question_number += 1
-            self.pose_question_and_wait_for_answer()
+            if(self.is_alive()):
+                self.current_question_number += 1
+                self.pose_question_and_wait_for_answer()
+
+   
+    def is_alive(self):
+        is_alive=True
+        if(self.nbVie==0) :
+            is_alive=False
+            self.isFinish.set()
+        return is_alive
         
 
     def afficherReponses(self, reponses):
         for i, reponse in enumerate(reponses):
             print(f"{i + 1}. {reponse[1]}")
     
-    def isGoodReponse(text_response,id_current_question):
-        id_current_response=get_id_reponse_for_text(text_response)
+    def isGoodReponse(id_current_response,id_current_question):
         id_good_reponse = getGoodResponseForQuestion(id_current_question)
         
         return id_good_reponse==id_current_response 
@@ -83,8 +93,8 @@ class Game:
             if self.current_question_number <= len(self.questions):
                 current_question = self.questions[self.current_question_number-1]
                 self.display_question_and_options(current_question)
-                
-                user_answer = self.get_user_answer_with_timeout()
+                time_for_question=get_time_question(current_question[0])
+                user_answer = self.get_user_answer_with_timeout(time_for_question)
                 
                 if user_answer:
                     self.process_answer(user_answer, current_question)
@@ -96,27 +106,46 @@ class Game:
                 self.isFinish.set()
 
     def display_question_and_options(self, question):
+        time_for_question=get_time_question(question[0])
+        self.display_time_life(time_for_question)
         print(f"\nQuestion {self.current_question_number}: {question[1]}")
+        
+        
         responses = getReponsesForQuestion(question[0])
         self.afficherReponses(responses)
 
-    def get_user_answer_with_timeout(self):
+    def display_time_life(self,time_for_question):
+        print(f'\n Tu as {time_for_question} secondes pour répondre')
+        print(f'Il te reste {self.nbVie} vies')
+
+
+    def get_user_answer_with_timeout(self,time_for_question):
         hasAnswer = Event()
         fn = sys.stdin.fileno()
         shared_value = Manager().Value(c_char_p, "")
         process_input = Process(target=Game.input_question, args=[fn, hasAnswer, shared_value])
-        timeout = Game.QUESTION_TIME
+        timeout = time_for_question
+        start_time = time.time()  
+
         process_input.start()
         hasAnswer.wait(timeout)
         process_input.terminate()
+
+        end_time = time.time()  
+
+        elapsed_time = end_time - start_time  
+        self.current_time_question = math.ceil(elapsed_time) if (elapsed_time - int(elapsed_time)) > 0.5 else int(elapsed_time)  # Arrondi
         return shared_value.value
 
     def process_answer(self, user_answer, current_question):
         response_index = int(user_answer) - 1
-        text_response = getReponsesForQuestion(current_question[0])[response_index][1]
-        if Game.isGoodReponse(text_response, current_question[0]):
+        id_response = getReponsesForQuestion(current_question[0])[response_index][0] # va chercher l'id de la réponse choisi
+        if Game.isGoodReponse(id_response, current_question[0]):
             self.score += get_point_question(current_question[0])
+        else :
+            self.nbVie-=1    
         self.score_max+=get_point_question(current_question[0])
+        self.global_time+=self.current_time_question
 
 
 
@@ -143,6 +172,9 @@ class Game:
                 print("Veuillez entrer un nombre valide.")
         
         return idTheme
+    def display_score(self):
+        print (f'Tu as pris {self.current_time_question} secondes à répondre à la question et tu es à {self.score}/{self.score_max}')
+        
     
     def set_questions_for_theme(self,id_theme):
         self.theme=id_theme
@@ -163,10 +195,12 @@ class Game:
 
         # Tant que le timer global n'est pas terminé
         while not isFinish.is_set():
-            # Poser la première question
-            self.next_question()     
+            # Poser la prochaine question
+            self.display_score()    
+            self.next_question()   
+              
 
-        print(f'Merci d avoir joué , tu as eu un score de {self.score}/{self.score_max}')   
+        print(f'Merci d avoir joué , tu as eu un score de {self.score}/{self.score_max} en {self.global_time} secondes ')   
            
             
 
